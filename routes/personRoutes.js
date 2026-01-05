5f9dc6b07a4c6784a1a88c6c9592404e7160ac43
Johnny watts <178673553+jwatts201708@users.noreply.github.com>
2026-01-05 14:15:01 -0500

    Add person routes for getting and creating people

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { getPeople, createPerson } from "../controllers/personController.js";
+
+const router = express.Router();
+
+router.get("/", getPeople);
+router.post("/", createPerson);
+
+export default router;
 
